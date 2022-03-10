f2a94d541d6d02707cfefb3907602dec0a3521aa
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-03-09 23:01:16 -0700

    add prompt

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var specialChar = "!@#$%^&*<>?_+~-".split("");
 var numbers = "0123456789".split("");
 
 
+
 function generatePassword() {
   console.log("Your Button Works!")
 
@@ -16,34 +17,37 @@ let pwLength = prompt("Choose the length you want your password to be. It must b
 if (!pwLength) {
   return;
 }
+//If a number not in range is entered will alert the following message
 if (pwLength < 8 || pwLength > 128) {
-alert("Really? Let's try this again. Click Generate, then enter a number between 8 and 128.")
+prompt("Really? Let's try this again. Click Generate, then enter a number between 8 and 128.")
 }
 {
 alert("Sweet! Let's choose some characters to use in your password. Use at least one! Press OK!")
 }
+//asks user if they want lower case letters
 let yesLowLetters = confirm("Lower case letters sound cool to use, click OK to include these.");
 if (yesLowLetters == true) {
   alert("Awesome! ✨");
 } else {
   alert("😕 No problem, we can use other characters.")
 }
-console.log(password);
+///asks user if they want upper case letters
 let yesCapLetters = confirm("Do upper case letters sound like something you want to use? Click OK");
 if (yesCapLetters == true) {
   alert("Fire! 🔥");
 } else {
   alert("🤨 Alright, we can use other characters.")
 }
+//asks user if they want numbers
 let yesNumbers = confirm("How about we toss in some nifty numbers? Click OK");
 if (yesNumbers == true) {
   alert("Piece of cake! 🍰");
 } else {
   alert("🙄 Okay, we can use other characters.")
 }
+//asks user if they want special characters
 let yesSpecialChar = confirm("Special characters are pretty gnarly, press OK to include these");
 if (yesSpecialChar == true) {
-
   alert("You are cooler than 💩!");
 } else {
   alert("🥱 Fine, but I hope you chose at least one set of characters.")
@@ -51,6 +55,7 @@ if (yesSpecialChar == true) {
 if (!yesLowLetters && !yesCapLetters && !yesNumbers && !yesSpecialChar) {
   alert("Please select at least one criteria!")
 }
+
   return "the dang password";
 }
 // Write password to the #password input

d26f45f04d277c3bf094b2795586ae5028229abd
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-03-09 21:38:03 -0700

    add alert for no options chosen

--- script.js
+++ script.js
@@ -7,43 +7,51 @@ var specialChar = "!@#$%^&*<>?_+~-".split("");
 var numbers = "0123456789".split("");
 
 
-
 function generatePassword() {
   console.log("Your Button Works!")
 
-//window prompt  
+//window prompt - length of pw 
 let pwLength = prompt("Choose the length you want your password to be. It must be between 8 and 128 characters");
+//is user presses cancel, function ends
+if (!pwLength) {
+  return;
+}
 if (pwLength < 8 || pwLength > 128) {
 alert("Really? Let's try this again. Click Generate, then enter a number between 8 and 128.")
 }
 {
-alert("sweet! Let's choose some characters to use in your password. Use at least one! Press OK!")
+alert("Sweet! Let's choose some characters to use in your password. Use at least one! Press OK!")
 }
-let lowLetters = confirm("Lower case letters sound cool to use, click OK to include these.");
-if (lowLetters) {
+let yesLowLetters = confirm("Lower case letters sound cool to use, click OK to include these.");
+if (yesLowLetters == true) {
   alert("Awesome! ✨");
 } else {
   alert("😕 No problem, we can use other characters.")
 }
-let capLetters = confirm("Do upper case letters sound like something you want to use?");
-if (capLetters) {
+console.log(password);
+let yesCapLetters = confirm("Do upper case letters sound like something you want to use? Click OK");
+if (yesCapLetters == true) {
   alert("Fire! 🔥");
 } else {
   alert("🤨 Alright, we can use other characters.")
 }
-let numbers = confirm("How about we toss in some nifty numbers?");
-if (numbers) {
+let yesNumbers = confirm("How about we toss in some nifty numbers? Click OK");
+if (yesNumbers == true) {
   alert("Piece of cake! 🍰");
 } else {
   alert("🙄 Okay, we can use other characters.")
 }
-let specialChar = confirm("Special characters are pretty gnarly, want to include these?");
-if (specialChar) {
+let yesSpecialChar = confirm("Special characters are pretty gnarly, press OK to include these");
+if (yesSpecialChar == true) {
+
   alert("You are cooler than 💩!");
 } else {
   alert("🥱 Fine, but I hope you chose at least one set of characters.")
 }
-  return "generated password";
+if (!yesLowLetters && !yesCapLetters && !yesNumbers && !yesSpecialChar) {
+  alert("Please select at least one criteria!")
+}
+  return "the dang password";
 }
 // Write password to the #password input
 //calls the writePassword function

4bff7031da112b1a1e03e551c870c01896a2b24c
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-03-11 16:46:21 -0700

    working code!

--- script.js
+++ script.js
@@ -18,53 +18,56 @@ if (!pwLength) {
 }
 //If a number not in range is entered will alert the following message
 if (pwLength < 8 || pwLength > 128) {
-prompt("Really? Let's try this again. Click Generate, then enter a number between 8 and 128.")
+prompt("Really? Let's try this again. Click Generate, then enter a number between 8 and 128.");
 }
 {
-alert("Sweet! Let's choose some characters to use in your password. Use at least one! Press OK!")
+alert("Sweet! Let's choose some characters to use in your password. Use at least one! Press OK!");
 }
 //asks user if they want lower case letters
 let yesLowLetters = confirm("Lower case letters sound cool to use, click OK to include these.");
 if (yesLowLetters == true) {
   alert("Awesome! ✨");
 } else {
-  alert("😕 No problem, we can use other characters.")
+  alert("😕 No problem, we can use other characters.");
 }
-var yesLowCase = lowLetters.concat(lowLetters);
 ///asks user if they want upper case letters
 let yesCapLetters = confirm("Do upper case letters sound like something you want to use? Click OK");
 if (yesCapLetters == true) {
   alert("Fire! 🔥");
 } else {
-  alert("🤨 Alright, we can use other characters.")
+  alert("🤨 Alright, we can use other characters.");
 }
-var yesCapCase = capLetters.concat(capLetters);
 //asks user if they want numbers
 let yesNumbers = confirm("How about we toss in some nifty numbers? Click OK");
 if (yesNumbers == true) {
   alert("Piece of cake! 🍰");
 } else {
-  alert("🙄 Okay, we can use other characters.")
+  alert("🙄 Okay, we can use other characters.");
 }
-var yesNum = numbers.concat(numbers);
 //asks user if they want special characters
 let yesSpecialChar = confirm("Special characters are pretty gnarly, press OK to include these");
 if (yesSpecialChar == true) {
   alert("You are cooler than 💩!");
 } else {
-  alert("🥱 Fine, but I hope you chose at least one set of characters.")
+  alert("🥱 Fine, but I hope you chose at least one set of characters.");
 }
-var yesSpecial = specialChar.concat(specialChar);
 if (!yesLowLetters && !yesCapLetters && !yesNumbers && !yesSpecialChar) {
-  alert("Please select at least one criteria!")
+  alert("Please select at least one criteria!");
 }
-let charOptions = [].concat(yesCapCase, yesLowCase, yesNum, yesSpecial);
+let charOptions = [].concat(lowLetters, capLetters, numbers, specialChar);
 console.log(charOptions);
-var theGeneratedOne = [];
+
+charOptions = charOptions.concat(specialChar);
+charOptions = charOptions.concat(capLetters);
+charOptions = charOptions.concat(numbers);
+charOptions = charOptions.concat(specialChar);
+
+
+var theGeneratedOne = '';
 for (var i = 0; i <= pwLength; i++) {
-  theGeneratedOne +- charOptions[Math.floor(Math.random() * charOptions.length)];
+  theGeneratedOne = theGeneratedOne + charOptions[Math.floor(Math.random() * charOptions.length)];
  }
- return theGeneratedOne.join('');
+ return theGeneratedOne;
 }
 // Write password to the #password input
 //calls the writePassword function
